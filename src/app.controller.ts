f56dbfe00fb468436c9b99363790defba80d130e
starci183 <cuongnvtse160875@gmail.com>
2025-05-24 21:26:36 +0700

    123

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/greet")
+  getGreet(): string {
+    return "greetings from the NestJS application!";
+  }
 }
